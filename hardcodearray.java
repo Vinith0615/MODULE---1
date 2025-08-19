f8be9919e96ff3c83d1957f4e46c754b1260bccb
VINITH S <vinithkingslayer@gmail.com>
2025-08-19 23:12:50 +0530

    Add files via upload

---
+++ hardcodearray.java
@@ -0,0 +1,14 @@
+//creating an array of size[10] and hardcoding it 
+public class hardcodearray {
+    public static void main(String[] args){
+        int arr[]={5,10,15,20,25,30,35,40,45,50};
+
+        for(int i =0;i<10;i++) {
+            System.out.println("Element at index "+i+" :  "+arr[i]);
+        }
+
+
+        
+    }
+    
+}
