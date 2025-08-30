5d93d6bcdfc3c4443d655e0cca1a8a9fc85da3ca
VINITH S <vinithkingslayer@gmail.com>
2025-08-30 20:39:03 +0530

    Add files via upload

---
+++ _02reverse.java
@@ -0,0 +1,12 @@
+//_02_Print all elements of the array in reverse order.
+public class _02reverse {
+    public static void main(String[] args) {
+        int[] rev={22,34,54,63,28};
+        for(int i =rev.length-1;i>=0;i--){
+            System.out.println(rev[i]);
+        }
+    }
+
+    
+    
+}
