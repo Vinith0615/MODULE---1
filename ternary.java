c1ddb01ed31c3afb48346a5b1b9720de491e8c0d
VINITH S <vinithkingslayer@gmail.com>
2025-09-16 21:12:57 +0530

    Add files via upload

---
+++ ternary.java
@@ -0,0 +1,9 @@
+//to cheack if a number is even or odd by ternary operator
+    public class ternary {
+        public static void main(String[] args) {
+            int number=10;
+            String result=(number % 2==0)?"Even":"odd";
+            System.out.println("The number "+ number +" is " + result);
+        }
+        
+    }
