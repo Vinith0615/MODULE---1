f9b47e5330de6cfdacc7a75a0441189c52ce4d95
VINITH S <vinithkingslayer@gmail.com>
2025-09-24 15:54:34 +0530

    Add files via upload

---
+++ nestedifexample.java
@@ -0,0 +1,21 @@
+class nestedifexample{
+    public static void main(String[] args) {
+        int marks=85;
+        if(marks >=50) {
+            System.out.println("you passed the exam ");
+
+            if(marks >=90) {
+                System.out.println("Grade : A+");
+
+            }else if(marks>=75) {
+                System.out.println("Grade : A");
+
+            }else{
+                System.out.println("Grade : B");
+            }
+
+        }else{
+            System.out.println("you failed");
+        }
+    }
+}
