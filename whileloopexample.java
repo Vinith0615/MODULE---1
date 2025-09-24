e18bb1ba42de2f2bf5302cdc4b010a556913ac4f
VINITH S <vinithkingslayer@gmail.com>
2025-09-24 16:07:01 +0530

    Add files via upload

---
+++ whileloopexample.java
@@ -0,0 +1,12 @@
+public class whileloopexample {
+    public static void main(String[] args) {
+        int number=12345;
+        int sum=0;
+        while(number>0) {
+            int digit=number%10;
+            sum+=digit;
+            number=number/10;
+        }
+        System.out.println("sum of digits= " +sum);
+    }
+}
